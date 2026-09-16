b524e1c634b7e47734589314360d0345651f785d
Sami <102156149+55d41251@users.noreply.github.com>
2026-09-16 14:06:50 +0300

    update

--- job-app/backend/data/jobs.js
+++ job-app/backend/data/jobs.js
@@ -1,4 +1,4 @@
-const books = [
+const jobs = [
   {
     title: "Software Engineer",
     type: "Full-time",
@@ -133,5 +133,5 @@ const books = [
   },
 ];
 
-module.exports = books;
+module.exports = jobs;
 
